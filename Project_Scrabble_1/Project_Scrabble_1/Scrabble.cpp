429b7156cabeb28001a845deea5bc0df8c42a1fb
magdalenaTI <magitihomirova@abv.bg>
2021-01-07 17:30:50 +0200

    First steps

--- Project_Scrabble_1/Project_Scrabble_1/Scrabble.cpp
+++ Project_Scrabble_1/Project_Scrabble_1/Scrabble.cpp
@@ -1,18 +1,19 @@
 #include <iostream>
 #include <vector>
+#include <string>
 #include <fstream>
 using namespace std;
 
 
-void startNewGame(vector<string> dictionary ) { 
-  
+void startNewGame(vector<string> dictionary) {
+
 }
 
 void options() {
-
+   
 }
 void addNewWord() {
-
+  
 }
 void mainMenu(int userInput) {
     switch (userInput) {
@@ -20,16 +21,16 @@ void mainMenu(int userInput) {
     case 2: options(); break;
     case 3: addNewWord(); break;
     case 4: return; break;
-    default : break;
+    default: break;
     }
 }
 
 int main() {
-    
-   vector<string> dictionary;
+
+    vector<string> dictionary;
     fstream words;
 
-    words.open("words.txt",ios::in|ios::out);
+    words.open("words.txt", ios::in | ios::out);
     if (words.is_open())
     {
         cout << "Loading dictionary..." << endl;
@@ -42,13 +43,13 @@ int main() {
     }
     else
     {
-        cerr << "Error reading from file!" <<endl;
+        cerr << "Error reading from file!" << endl;
     }
 
 
     string userInput;
 
-    while (true) {
+    while (true) { 
         cout << "Press 1 to strat new game" << endl;
         cout << "Press 2 for options" << endl;
         cout << "Press 3 to add new word" << endl;
@@ -58,9 +59,10 @@ int main() {
         if (userInput == "2") { options(); }
         if (userInput == "3") { addNewWord(); }
         if (userInput == "4") { return 0; }
-        
+
     }
-   
-  
+
+    
 
 }
+
