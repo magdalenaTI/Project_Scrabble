c640755e0c772455493da703b1c60dabe541c82e
magdalenaTI <magitihomirova@abv.bg>
2021-01-08 17:49:00 +0200

    Functions corrections

--- Project_Scrabble_1/Project_Scrabble_1/Scrabble.cpp
+++ Project_Scrabble_1/Project_Scrabble_1/Scrabble.cpp
@@ -1,36 +1,73 @@
 #include <iostream>
 #include <vector>
 #include <string>
+#include <ctime>
 #include <fstream>
 using namespace std;
 
+string getLetters(int letters) {
 
-void startNewGame(vector<string> dictionary) {
+    srand(time(NULL));
+    string result;
+   
+    const int size = 10;
+    for (int i = 0; i < size; i++) {
+    result.push_back('a' + rand() % 26);
+    }
+    return result;
 
-}
-
-void options() {
    
 }
-void addNewWord() {
-  
+bool validation(vector<string> dictionary, char userInput[], string letters) {
+
 }
-void mainMenu(int userInput) {
-    switch (userInput) {
-    case 1: startNewGame(); break;
-    case 2: options(); break;
-    case 3: addNewWord(); break;
-    case 4: return; break;
-    default: break;
+
+
+void startNewGame(vector<string> dictionary, vector <int> gameOptions) {
+
+    int letters, rounds;
+    vector<string> dictionary;
+
+}
+
+
+void options(vector<int>& settings) {
+    
+}
+
+
+void addNewWord(vector<string>& dictionary, fstream words) {
+
+    string userInput;
+  
+    cout << "Enter your word";
+    cin >> userInput;
+    if (words.is_open())
+    {
+        if (find(dictionary.begin(), dictionary.end(), userInput) == dictionary.end())
+        {
+            dictionary.push_back(userInput);
+           
+            words.open("words.txt", ios::app);
+
+            words << userInput;
+        }
     }
+    else
+    {
+        cerr << "Error reading from file!" << endl;
+       
+    }
+   
 }
 
 int main() {
 
     vector<string> dictionary;
+
     fstream words;
 
-    words.open("words.txt", ios::in | ios::out);
+    words.open("words.txt", ios::in | ios::app);
     if (words.is_open())
     {
         cout << "Loading dictionary..." << endl;
@@ -46,14 +83,14 @@ int main() {
         cerr << "Error reading from file!" << endl;
     }
 
-
     string userInput;
 
-    while (true) { 
-        cout << "Press 1 to strat new game" << endl;
-        cout << "Press 2 for options" << endl;
-        cout << "Press 3 to add new word" << endl;
-        cout << "Press 4 to finish" << endl;
+    while (true) {
+        cout << "Press 1 to start a new game." << endl;
+        cout << "Press 2 to change game options." << endl;
+        cout << "Press 3 to add a new word." << endl;
+        cout << "Press 4 to exit the game." << endl;
+
         cin >> userInput;
         if (userInput == "1") { startNewGame(); }
         if (userInput == "2") { options(); }
@@ -62,7 +99,6 @@ int main() {
 
     }
 
-    
-
+    vector<int>settings;
 }
 
