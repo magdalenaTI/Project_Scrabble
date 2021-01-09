5cdecf8458ced847eaffb26be49fcc8aceabbd2e
magdalenaTI <magitihomirova@abv.bg>
2021-01-09 21:52:34 +0200

    Function extentions

--- Project_Scrabble_1/Project_Scrabble_1/Scrabble.cpp
+++ Project_Scrabble_1/Project_Scrabble_1/Scrabble.cpp
@@ -5,71 +5,126 @@
 #include <fstream>
 using namespace std;
 
-string getLetters(int letters) {
+string getLetters(int lettersCount) {
 
-    srand(time(NULL));
+    srand((unsigned int)time(NULL));
     string result;
-   
-    const int size = 10;
-    for (int i = 0; i < size; i++) {
-    result.push_back('a' + rand() % 26);
+    int numberOfVowels = 3;
+    for (int i = 0; i < lettersCount; i++) {
+        char letters = 'a' + rand() % 26;
+        result.push_back('a' + rand() % 26);
     }
     return result;
+}
 
+bool validation(vector<string> dictionary, string userInput, string letters) {
+    if (userInput.length() == 0) return false;
    
-}
-bool validation(vector<string> dictionary, char userInput[], string letters) {
+
+    int histogramLetters[26] = { 0 };
+    int histogramUserInput[26] = { 0 };
+
+    for (auto it = letters.cbegin(); it != letters.cend(); it++) {
+        histogramLetters[(int)(*it - 'a')]++;
+
+    }
+    for (auto it = userInput.cbegin(); it != userInput.cend(); it++) {
+        histogramUserInput[(int)(*it - 'a')]++;
+
+    }
+    for (int i = 0; i < 26; i++) {
+        if (histogramLetters[i] < histogramUserInput[i]) {
+            cout << "Your word is not a valid one! Try again with the letters given!"<< endl;
+            return false;
+        }
+    }
+
+    if (find(dictionary.begin(), dictionary.end(), userInput) == dictionary.end()) { 
+        
+        cout << " The word is not found in the dictionary!" << endl;
+        return false; 
+    }
+     
+    return true;
 
 }
 
+void startNewGame(vector<string> dictionary, vector<int> settings) {
 
-void startNewGame(vector<string> dictionary, vector <int> gameOptions) {
+    int points = 0;
+    int lettersCount = settings[0];
+    int roundsCount= settings[1];
+    for (int i = 0; i < roundsCount; i++) {
+        string letters = getLetters(lettersCount);
+        cout << "You have the following letters: ";
+        for (int j = 0; j < lettersCount; j++) {
+            cout << letters[j] << " ";
+        }
 
-    int letters, rounds;
-    vector<string> dictionary;
+        string userInput;
+        cout << "Enter a word: "; 
+        cin >> userInput; 
+    
+        while (!validation(dictionary, userInput, letters)) {
+          
+            cout << "You have the following letters: ";
+            for (int j = 0; j < lettersCount; j++) {
+                cout << letters[j] << " ";
+            }
+            cout << "Enter a word: ";
+            cin >> userInput;
+       }
+        points += userInput.length();
+        cout << "Your points are: " << points << endl;
+
+    }
+
+    cout << "You finished! Your points are: " << points << endl;
 
 }
 
 
 void options(vector<int>& settings) {
+    cout << "What would you like to change?"; 
     
+        char answerUser;
 }
 
+void addNewWord(vector<string>& dictionary, fstream& words) {
 
-void addNewWord(vector<string>& dictionary, fstream words) {
+    
+    string userInput; 
 
-    string userInput;
-  
     cout << "Enter your word";
     cin >> userInput;
+
+    
     if (words.is_open())
     {
         if (find(dictionary.begin(), dictionary.end(), userInput) == dictionary.end())
         {
             dictionary.push_back(userInput);
-           
-            words.open("words.txt", ios::app);
 
             words << userInput;
+            cout << "The word was successfully added!" << endl;
         }
     }
     else
     {
-        cerr << "Error reading from file!" << endl;
-       
+        cerr << "Error writing to file!" << endl;
+        
     }
    
 }
 
 int main() {
-
     vector<string> dictionary;
 
     fstream words;
-
     words.open("words.txt", ios::in | ios::app);
     if (words.is_open())
     {
+       
         cout << "Loading dictionary..." << endl;
         string word;
         while (getline(words, word))
@@ -83,7 +138,11 @@ int main() {
         cerr << "Error reading from file!" << endl;
     }
 
+
     string userInput;
+    vector<int>settings;
+    settings.push_back(10);
+    settings.push_back(10);
 
     while (true) {
         cout << "Press 1 to start a new game." << endl;
@@ -92,13 +151,12 @@ int main() {
         cout << "Press 4 to exit the game." << endl;
 
         cin >> userInput;
-        if (userInput == "1") { startNewGame(); }
-        if (userInput == "2") { options(); }
-        if (userInput == "3") { addNewWord(); }
-        if (userInput == "4") { return 0; }
-
+        if (userInput == "1") { startNewGame(dictionary, settings); }
+        else if (userInput == "2") { options(settings); }
+        else if (userInput == "3") { addNewWord(dictionary, words); }
+        else if (userInput == "4") { return 0; }
+        else cout << "Wrong input! Enter again!" << endl;
     }
 
-    vector<int>settings;
 }
 
